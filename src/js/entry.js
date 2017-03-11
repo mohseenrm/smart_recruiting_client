14afa0e5880ad54bd84e00e6d383e1930dbc529a
mohseenrm <mohseenmukaddam6@gmail.com>
2017-03-11 01:38:01 -0700

    react ftw

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,6 +1,10 @@
 'use strict';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.jsx';
 
 (()=>{
     const name = 'MoMo';
     console.log(`Hello ${name}! from webpack`);
+	ReactDOM.render(<App />, document.getElementById('app'));
 })();
