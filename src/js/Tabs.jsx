98c8bb96e57dd42121dc0f6089dee69e8337c57d
mohseenrm <mohseenmukaddam6@gmail.com>
2017-03-11 19:40:14 -0700

    saving changes

---
+++ src/js/Tabs.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+import React from 'react';
+
+export default class Tabs extends React.Component{
+	getDefaultProps(){
+		return{
+			selected: 0
+		};
+	}
+	getInitialState(){
+		
+	}
+}
